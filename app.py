886ff4b86e37502d3713713fe70a8bcfa6f33d92
key <30455795+keyhsw@users.noreply.github.com>
2023-11-16 19:21:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import gradio as gr
+from XEdu.hub import Workflow as wf
+model = wf(task='MMEdu',checkpoint='model/Oracle.onnx')
 
 def predict(input_img):
-    # result = model.inference(data=input_img) 
-    # result = model.format_output(lang="zh")
-    return input_img,None
+    result = model.inference(data=input_img) 
+    result = model.format_output(lang="zh")
+    return input_img,result
 
 demo = gr.Interface(fn=predict,  inputs=gr.Image(shape=(128, 128),source="canvas"), outputs=["image","text"])
-if __name__ == "__main__":
-    demo.launch()
+demo.launch(share=True)
