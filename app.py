9f401bef84ca875612f30475d92592475b0983bd
key <30455795+keyhsw@users.noreply.github.com>
2023-11-16 19:16:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import gradio as gr
-# from XEdu.hub import Workflow as wf
-# model = wf(task='MMEdu',checkpoint='model/Oracle.onnx')
 
 def predict(input_img):
     # result = model.inference(data=input_img) 
@@ -8,4 +6,5 @@ def predict(input_img):
     return input_img,None
 
 demo = gr.Interface(fn=predict,  inputs=gr.Image(shape=(128, 128),source="canvas"), outputs=["image","text"])
-demo.launch()
+if __name__ == "__main__":
+    demo.launch()
