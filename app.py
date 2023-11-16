0c688c33410419ce90634b327f96dad5eaec4337
key <30455795+keyhsw@users.noreply.github.com>
2023-11-16 18:47:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ model = wf(task='MMEdu',checkpoint='model/Oracle.onnx')
 def predict(input_img):
     # result = model.inference(data=input_img) 
     # result = model.format_output(lang="zh")
-    return input_img,result
+    return input_img,None
 
 demo = gr.Interface(fn=predict,  inputs=gr.Image(shape=(128, 128),source="canvas"), outputs=["image","text"])
 demo.launch(share=True)
