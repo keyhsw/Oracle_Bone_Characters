de803b7d1350be477769eee1b62482ee9fbad13a
key <30455795+keyhsw@users.noreply.github.com>
2023-11-16 18:45:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from XEdu.hub import Workflow as wf
 model = wf(task='MMEdu',checkpoint='model/Oracle.onnx')
 
 def predict(input_img):
-    result = model.inference(data=input_img) 
-    result = model.format_output(lang="zh")
+    # result = model.inference(data=input_img) 
+    # result = model.format_output(lang="zh")
     return input_img,result
 
-demo = gr.Interface(fn=predict,  inputs=gr.Image(shape=(128, 128),source="canvas"),"image", outputs=["image","text"])
+demo = gr.Interface(fn=predict,  inputs=gr.Image(shape=(128, 128),source="canvas"), outputs=["image","text"])
 demo.launch(share=True)
